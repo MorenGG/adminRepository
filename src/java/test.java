f68eddadc1f9ee11e36c05e8165847b0299becb2
zs <zd@bjpowernode.com>
2019-09-01 20:32:59 +0800

    初始導入

---
+++ src/java/test.java
@@ -0,0 +1,17 @@
+package java;/**
+ * ClassName:test
+ * Package:java
+ * Description:<br/>
+ *
+ * @date:2019/9/1 0001 17:48
+ * @author:lrz
+ */
+
+/**
+ * Author lrz
+ */
+public class test {
+    private String name;
+    private String age;
+    private String gender;
+}
